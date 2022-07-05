628eaf338e2ee95c058c984a56bbdc6ca9aeddc5
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-07-06 05:50:50 +0700

    update: menambahkan _document.js

---
+++ src/pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Document = () => (
+  <Html>
+    <Head>
+      <link
+        href="https://fonts.googleapis.com/css?family=Lexend+Deca:100,200,300,regular,500,600,700,800,900"
+        rel="stylesheet"
+      />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
