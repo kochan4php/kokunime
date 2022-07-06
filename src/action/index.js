d445fd3ef51bb1d1fe09cd2945cb4126c9fd0700
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-07-06 20:04:30 +0700

    update: perbaikan pada saat pemanggilan api

--- src/action/index.js
+++ src/action/index.js
@@ -2,40 +2,48 @@ import KUSONIME_API from "../config/kusonime";
 
 export const getAnimeDetail = async (slug) => {
   const res = await fetch(`${KUSONIME_API}/anime/${slug}`);
-  return res.ok ? res.json() : undefined;
+  if (res.ok) return res.json();
+  else return undefined;
 };
 
-export const getAnimeWithPagination = async (page = undefined) => {
+export const getAnimeWithPagination = async (page) => {
   const res = await fetch(`${KUSONIME_API}/page/${page ?? 1}`);
-  return res.ok ? res.json() : undefined;
+  if (res.ok) return res.json();
+  else return undefined;
 };
 
 export const getAnimeRecommendations = async () => {
   const res = await fetch(`${KUSONIME_API}/rekomendasi`);
-  return res.ok ? res.json() : undefined;
+  if (res.ok) return res.json();
+  else return undefined;
 };
 
 export const getAnimeSearch = async (keyword) => {
   const res = await fetch(`${KUSONIME_API}/cari/${keyword}`);
-  return res.ok ? res.json() : undefined;
+  if (res.ok) return res.json();
+  else return undefined;
 };
 
 export const getGenreList = async () => {
   const res = await fetch(`${KUSONIME_API}/genres`);
-  return res.ok ? res.json() : undefined;
+  if (res.ok) return res.json();
+  else return undefined;
 };
 
 export const getGenre = async (genreName, page) => {
   const res = await fetch(`${KUSONIME_API}/genres/${genreName}/${page ?? 1}`);
-  return res.ok ? res.json() : undefined;
+  if (res.ok) return res.json();
+  else return undefined;
 };
 
 export const getSeasonList = async () => {
   const res = await fetch(`${KUSONIME_API}/seasons`);
-  return res.ok ? res.json() : undefined;
+  if (res.ok) return res.json();
+  else return undefined;
 };
 
 export const getSeason = async (seasonName, page) => {
   const res = await fetch(`${KUSONIME_API}/seasons/${seasonName}/${page ?? 1}`);
-  return res.ok ? res.json() : undefined;
+  if (res.ok) return res.json();
+  else return undefined;
 };
