c69ca20236bbb54fdb2fb66ebfe4c985e2ba547c
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-07-06 22:39:11 +0700

    update: mempersingkat syntax untuk pemanggilan api

--- src/action/index.js
+++ src/action/index.js
@@ -1,46 +1,26 @@
 import axios from "axios";
 import KUSONIME_API from "../config/kusonime";
 
-export const getAnimeDetail = async (slug) => {
-  const res = await axios.get(`${KUSONIME_API}/anime/${slug}`);
-  return res;
-};
+export const getAnimeDetail = async (slug) =>
+  await axios.get(`${KUSONIME_API}/anime/${slug}`);
 
-export const getAnimeWithPagination = async (page) => {
-  const res = await axios.get(`${KUSONIME_API}/page/${page ?? 1}`);
-  return res;
-};
+export const getAnimeWithPagination = async (page) =>
+  await axios.get(`${KUSONIME_API}/page/${page ?? 1}`);
 
-export const getAnimeRecommendations = async () => {
-  const res = await axios.get(`${KUSONIME_API}/rekomendasi`);
-  return res;
-};
+export const getAnimeRecommendations = async () =>
+  await axios.get(`${KUSONIME_API}/rekomendasi`);
 
-export const getAnimeSearch = async (keyword) => {
-  const res = await axios.get(`${KUSONIME_API}/cari/${keyword}`);
-  return res;
-};
+export const getAnimeSearch = async (keyword) =>
+  await axios.get(`${KUSONIME_API}/cari/${keyword}`);
 
-export const getGenreList = async () => {
-  const res = await axios.get(`${KUSONIME_API}/genres`);
-  return res;
-};
+export const getGenreList = async () =>
+  await axios.get(`${KUSONIME_API}/genres`);
 
-export const getGenre = async (genreName, page) => {
-  const res = await axios.get(
-    `${KUSONIME_API}/genres/${genreName}/${page ?? 1}`
-  );
-  return res;
-};
+export const getGenre = async (genreName, page) =>
+  await axios.get(`${KUSONIME_API}/genres/${genreName}/${page ?? 1}`);
 
-export const getSeasonList = async () => {
-  const res = await axios.get(`${KUSONIME_API}/seasons`);
-  return res;
-};
+export const getSeasonList = async () =>
+  await axios.get(`${KUSONIME_API}/seasons`);
 
-export const getSeason = async (seasonName, page) => {
-  const res = await axios.get(
-    `${KUSONIME_API}/seasons/${seasonName}/${page ?? 1}`
-  );
-  return res;
-};
+export const getSeason = async (seasonName, page) =>
+  await axios.get(`${KUSONIME_API}/seasons/${seasonName}/${page ?? 1}`);
