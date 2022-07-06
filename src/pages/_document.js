b94fee6663e79e3a8e6d0db01204e9c749e9a06e
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-07-06 20:23:48 +0700

    update: perbaikan kecil

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,7 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 const Document = () => (
-  <Html>
+  <Html lang="en">
     <Head>
       <link
         href="https://fonts.googleapis.com/css?family=Lexend+Deca:100,200,300,regular,500,600,700,800,900"
