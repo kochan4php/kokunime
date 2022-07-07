90b817bd2e527f483515d4ad3ea365b4310b8221
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-07-07 13:57:35 +0700

    update: perbaikan layout pada halaman detail anime

--- src/pages/details/[slug].js
+++ src/pages/details/[slug].js
@@ -40,16 +40,16 @@ const DetailAnime = () => {
       <RenderIfFalse isFalse={isLoading}>
         <RenderIfTrue isTrue={Object.keys(detailAnime).length > 0}>
           <div className="container mt-2 mb-6 p-4">
-            <div className="grid grid-cols-1 md:grid-cols-3">
-              <div className="flex justify-center items-center selection:bg-pink-500">
+            <div className="grid grid-cols-1 lg:grid-cols-3">
+              <div className="flex justify-center items-center selection:bg-pink-500 mb-5 lg:mb-0">
                 <ParallaxImage
                   image={detailAnime?.thumbnail}
                   alt={detailAnime?.title}
                 />
               </div>
-              <div className="flex flex-col justify-start md:col-start-2 md:col-end-4 p-4 md:py-0">
+              <div className="flex flex-col justify-start lg:col-start-2 lg:col-end-4 p-4 lg:py-0">
                 <RenderIfTrue isTrue={detailAnime?.score}>
-                  <span className="gap-3 mt-3 md:mt-0 md:mb-4 text-2xl md:text-xl lg:text-2xl flex items-center selection:bg-emerald-500 selection:text-emerald-900">
+                  <span className="gap-3 mt-3 md:mt-0 md:mb-4 text-2xl flex items-center selection:bg-emerald-500 selection:text-emerald-900">
                     <span className="text-3xl text-yellow-500">⭐</span>{" "}
                     <span>{detailAnime?.score?.substring(1)}</span>
                   </span>
@@ -58,69 +58,82 @@ const DetailAnime = () => {
                   {detailAnime?.title}
                 </h1>
                 <div className="text-lg selection:bg-pink-500 selection:text-pink-900">
-                  <RenderIfTrue isTrue={detailAnime?.japanese}>
-                    <Text category="Japanese">{detailAnime?.japanese}</Text>
-                  </RenderIfTrue>
-                  <RenderIfTrue isTrue={detailAnime?.release}>
-                    <Text category="Release">{detailAnime?.release}</Text>
-                  </RenderIfTrue>
-                  <RenderIfTrue isTrue={detailAnime?.season?.name}>
-                    <Text category="Season"> {detailAnime?.season?.name}</Text>
-                  </RenderIfTrue>
-                  <RenderIfTrue isTrue={detailAnime?.genre}>
-                    <Text category="Genre">
-                      <For
-                        each={detailAnime?.genre}
-                        render={(genre, index) => {
-                          const coma =
-                            detailAnime?.genre.length - 1 === index ? "" : ", ";
-                          return (
-                            <Link
-                              href={`/${genre.endpoint}`}
-                              key={index}
-                              passHref
-                            >
-                              <a className="hover:text-pink-500 transition-colors duration-300">
-                                {genre.name}
-                                {coma}
-                              </a>
-                            </Link>
-                          );
-                        }}
-                      />
-                    </Text>
-                  </RenderIfTrue>
-                  <RenderIfTrue isTrue={detailAnime?.total_eps}>
-                    <Text category="Total Eps">{detailAnime?.total_eps}</Text>
-                  </RenderIfTrue>
-                  <RenderIfTrue isTrue={detailAnime?.durasi}>
-                    <Text category="Duration">{detailAnime?.durasi}</Text>
-                  </RenderIfTrue>
-                  <RenderIfTrue isTrue={detailAnime?.producers}>
-                    <Text category="Producers">
-                      <For
-                        each={detailAnime?.producers}
-                        render={(producer, index) => {
-                          const coma =
-                            detailAnime?.producers.length - 1 === index
-                              ? ""
-                              : ", ";
-                          return (
-                            <span key={index}>
-                              {producer}
-                              {coma}
-                            </span>
-                          );
-                        }}
-                      />
-                    </Text>
-                  </RenderIfTrue>
-                  <RenderIfTrue isTrue={detailAnime?.type}>
-                    <Text category="Type">{detailAnime?.type}</Text>
-                  </RenderIfTrue>
-                  <RenderIfTrue isTrue={detailAnime?.status}>
-                    <Text category="Status"> {detailAnime?.status}</Text>
-                  </RenderIfTrue>
+                  <div className="lg:flex lg:justify-between lg:gap-3">
+                    <div className="lg:px-2">
+                      <RenderIfTrue isTrue={detailAnime?.japanese}>
+                        <Text category="Japanese">{detailAnime?.japanese}</Text>
+                      </RenderIfTrue>
+                      <RenderIfTrue isTrue={detailAnime?.release}>
+                        <Text category="Release">{detailAnime?.release}</Text>
+                      </RenderIfTrue>
+                      <RenderIfTrue isTrue={detailAnime?.season?.name}>
+                        <Text category="Season">
+                          {detailAnime?.season?.name}
+                        </Text>
+                      </RenderIfTrue>
+
+                      <RenderIfTrue isTrue={detailAnime?.total_eps}>
+                        <Text category="Total Eps">
+                          {detailAnime?.total_eps}
+                        </Text>
+                      </RenderIfTrue>
+                      <RenderIfTrue isTrue={detailAnime?.durasi}>
+                        <Text category="Duration">{detailAnime?.durasi}</Text>
+                      </RenderIfTrue>
+                    </div>
+                    <div className="lg:px-2 lg:w-2/3">
+                      <RenderIfTrue isTrue={detailAnime?.genre}>
+                        <Text category="Genre">
+                          <For
+                            each={detailAnime?.genre}
+                            render={(genre, index) => {
+                              const coma =
+                                detailAnime?.genre.length - 1 === index
+                                  ? ""
+                                  : ", ";
+                              return (
+                                <Link
+                                  href={`/${genre.endpoint}`}
+                                  key={index}
+                                  passHref
+                                >
+                                  <a className="hover:text-pink-500 transition-colors duration-300">
+                                    {genre.name}
+                                    {coma}
+                                  </a>
+                                </Link>
+                              );
+                            }}
+                          />
+                        </Text>
+                      </RenderIfTrue>
+                      <RenderIfTrue isTrue={detailAnime?.producers}>
+                        <Text category="Producers">
+                          <For
+                            each={detailAnime?.producers}
+                            render={(producer, index) => {
+                              const coma =
+                                detailAnime?.producers.length - 1 === index
+                                  ? ""
+                                  : ", ";
+                              return (
+                                <span key={index}>
+                                  {producer}
+                                  {coma}
+                                </span>
+                              );
+                            }}
+                          />
+                        </Text>
+                      </RenderIfTrue>
+                      <RenderIfTrue isTrue={detailAnime?.type}>
+                        <Text category="Type">{detailAnime?.type}</Text>
+                      </RenderIfTrue>
+                      <RenderIfTrue isTrue={detailAnime?.status}>
+                        <Text category="Status"> {detailAnime?.status}</Text>
+                      </RenderIfTrue>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
