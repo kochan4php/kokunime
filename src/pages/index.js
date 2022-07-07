f7a6c4d7753b4c3abffb33bdb4c881f46b7f4b9e
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-07-08 02:33:47 +0700

    update: perbaikan kecil

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,82 +1,103 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { getAnimeWithPagination } from "../action";
-import { AlertWarning, Button, MainCard, TitleSection } from "../components";
+import {
+  AlertWarning,
+  Button,
+  Loading,
+  MainCard,
+  TitleSection,
+} from "../components";
 import { For, RenderIfFalse, RenderIfTrue } from "../utils";
 
-export const getServerSideProps = async () => {
-  const res = await getAnimeWithPagination();
-
-  return {
-    props: {
-      res: res.data,
-    },
-  };
-};
-
-const Home = ({ res }) => {
+const Home = () => {
   const router = useRouter();
 
+  const [anime, setAnime] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getData = async () => {
+    const res = await getAnimeWithPagination();
+    if (res.status === 200) {
+      if (res.data.success !== false) {
+        console.log(res.data);
+        setAnime(res.data);
+        setIsLoading(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    getData();
+  }, []);
+
   return (
-    <>
-      <section
-        className="bg-slate-800 min-w-full bg-cover bg-center bg-no-repeat object-cover"
-        style={{
-          backgroundImage: `url(${res[0]?.link?.thumbnail})`,
-        }}
-      >
-        <section className="min-h-[40vh] sm:min-h-[50vh] md:min-h-[40vh] lg:min-h-[70vh] xl:min-h-[80vh] 2xl:min-h-[75vh] bg-slate-800 bg-opacity-40 backdrop-blur-lg backdrop-brightness-90 flex items-center justify-start px-2 md:px-6 pb-6">
-          <div className="container max-w-full lg:max-w-3xl">
-            <div className="my-6">
-              <h1 className="text-slate-50 text-2xl md:text-4xl font-semibold mb-4 selection:bg-red-700 selection:text-red-200">
-                {res[0]?.title}
-              </h1>
+    <section className="min-w-full bg-gradient-to-tl from-slate-900 via-slate-800 to-slate-900 text-white min-h-screen">
+      <RenderIfTrue isTrue={isLoading}>
+        <Loading />
+      </RenderIfTrue>
+      <RenderIfFalse isFalse={isLoading}>
+        <section
+          className="bg-slate-800 min-w-full bg-cover bg-center bg-no-repeat object-cover"
+          style={{
+            backgroundImage: `url(${anime[0]?.link?.thumbnail})`,
+          }}
+        >
+          <section className="min-h-[40vh] sm:min-h-[50vh] md:min-h-[40vh] lg:min-h-[70vh] xl:min-h-[80vh] 2xl:min-h-[75vh] bg-slate-800 bg-opacity-40 backdrop-blur-lg backdrop-brightness-90 flex items-center justify-start px-2 md:px-6 pb-6">
+            <div className="container max-w-full lg:max-w-3xl">
+              <div className="my-6">
+                <h1 className="text-slate-50 text-2xl md:text-4xl font-semibold mb-4 selection:bg-red-700 selection:text-red-200">
+                  {anime[0]?.title}
+                </h1>
+              </div>
+              <div className="my-6">
+                <p className="text-lg md:text-2xl text-slate-200 font-medium truncate selection:bg-teal-400 selection:text-teal-900">
+                  {anime[0]?.release}
+                </p>
+              </div>
+              <div className="my-6">
+                <Button
+                  onClick={() =>
+                    router.push(`/details/${anime[0]?.link?.endpoint}`)
+                  }
+                  width="w-full"
+                >
+                  Detail
+                </Button>
+              </div>
             </div>
-            <div className="my-6">
-              <p className="text-lg md:text-2xl text-slate-200 font-medium truncate selection:bg-teal-400 selection:text-teal-900">
-                {res[0]?.release}
-              </p>
-            </div>
-            <div className="my-6">
-              <Button
-                onClick={() =>
-                  router.push(`/details/${res[0]?.link?.endpoint}`)
-                }
-                width="w-full"
-              >
-                Detail
-              </Button>
-            </div>
+          </section>
+        </section>
+        <section className="min-h-screen min-w-full bg-gradient-to-tl from-slate-900 via-slate-800 to-slate-900 py-10">
+          <div className="mb-7 container">
+            <TitleSection>Updatetan Terbaru</TitleSection>
+          </div>
+          <div className="container px-0 md:px-4 min-h-screen">
+            <RenderIfTrue isTrue={anime.length > 0}>
+              <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 lg:gap-6">
+                <For
+                  each={anime}
+                  render={(data, index) => (
+                    <MainCard
+                      key={index}
+                      path={`/details/${data?.link?.endpoint}`}
+                      image={data?.link?.thumbnail}
+                      title={data?.title}
+                      py="py-5"
+                      fontsize="text-base"
+                    />
+                  )}
+                />
+              </div>
+            </RenderIfTrue>
+            <RenderIfFalse isFalse={anime.length > 0}>
+              <AlertWarning message="Gagal mengambil data dari API, coba refresh ulang browsernya" />
+            </RenderIfFalse>
           </div>
         </section>
-      </section>
-      <section className="min-h-screen min-w-full bg-gradient-to-tl from-slate-900 via-slate-800 to-slate-900 py-10">
-        <div className="mb-7 container">
-          <TitleSection>Updatetan Terbaru</TitleSection>
-        </div>
-        <div className="container px-0 md:px-4 min-h-screen">
-          <RenderIfTrue isTrue={res.length > 0}>
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 lg:gap-6">
-              <For
-                each={res}
-                render={(data, index) => (
-                  <MainCard
-                    key={index}
-                    path={`/details/${data?.link?.endpoint}`}
-                    image={data?.link?.thumbnail}
-                    title={data?.title}
-                    py="py-5"
-                    fontsize="text-base"
-                  />
-                )}
-              />
-            </div>
-          </RenderIfTrue>
-          <RenderIfFalse isFalse={res.length > 0}>
-            <AlertWarning message="Gagal mengambil data dari API, coba refresh ulang browsernya" />
-          </RenderIfFalse>
-        </div>
-      </section>
-    </>
+      </RenderIfFalse>
+    </section>
   );
 };
 
