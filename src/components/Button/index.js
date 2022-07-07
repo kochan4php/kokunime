129a896e50e6a838811b8bf6a7ffcdeda168ffd2
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-07-07 23:18:04 +0700

    update: perbaikan kecil

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const Button = ({ children, onClick, bgcolor, width }) => (
   <button
-    className={`px-7 py-1.5 rounded text-slate-5 active:ring active:ring-sky-500 hover:border-sky-500 border-2 border-transparent transition-all duration-300 selection:bg-orange-500 selection:text-orange-900 text-base md:text-lg ${bgcolor} ${width} h-full`}
+    className={`px-1 py-1.5 rounded text-slate-5 active:ring active:ring-sky-500 hover:border-sky-500 border-2 border-transparent transition-all duration-300 selection:bg-orange-500 selection:text-orange-900 text-base md:text-lg ${bgcolor} ${width} h-full`}
     onClick={onClick}
   >
     {children}
