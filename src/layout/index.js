7e236f7895e6b7d99c189afe7063a3f540357f4e
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-08-15 01:02:58 +0700

    fix: perbaikan kecil dan file readme

--- src/layout/index.js
+++ src/layout/index.js
@@ -6,10 +6,16 @@ const Layout = ({ children }) => (
   <>
     <Head>
       <title>Kokunime</title>
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+      />
     </Head>
     <div className="min-h-screen flex flex-col justify-between">
       <Navbar />
-      <main className="min-h-full">{children}</main>
+      <main className="min-h-full" id="particle-js">
+        {children}
+      </main>
       <Footer />
     </div>
   </>
