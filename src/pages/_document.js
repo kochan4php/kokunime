4efef612f3fac8cc1f0aafa387c6dd002f2e0c04
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-08-14 16:22:46 +0700

    update: change icon window

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -20,130 +20,130 @@ const Document = () => (
         type="image/png"
         sizes="16x16"
         rel="icon"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <link
         type="image/png"
         sizes="32x32"
         rel="icon"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <link
         type="image/png"
         sizes="96x96"
         rel="icon"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <link
         type="image/png"
         sizes="120x120"
         rel="icon"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="72x72"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="96x96"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="144x144"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="192x192"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="512x512"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <link
         rel="apple-touch-icon"
         type="image/png"
         sizes="57x57"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <link
         rel="apple-touch-icon"
         type="image/png"
         sizes="60x60"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <link
         rel="apple-touch-icon"
         type="image/png"
         sizes="72x72"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <link
         rel="apple-touch-icon"
         type="image/png"
         sizes="76x76"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <link
         rel="apple-touch-icon"
         type="image/png"
         sizes="114x114"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <link
         rel="apple-touch-icon"
         type="image/png"
         sizes="120x120"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <link
         rel="apple-touch-icon"
         type="image/png"
         sizes="144x144"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <link
         rel="apple-touch-icon"
         type="image/png"
         sizes="152x152"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <link
         rel="apple-touch-icon"
         type="image/png"
         sizes="180x180"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <link
         color="#26E07F"
         rel="mask-icon"
-        href="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <meta
         name="msapplication-square70x70logo"
-        content="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        content="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <meta
         name="msapplication-TileImage"
-        content="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        content="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <meta
         name="msapplication-square150x150logo"
-        content="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        content="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <meta
         name="msapplication-square310x310logo"
-        content="https://img.icons8.com/dotty/100/000000/anime-emoji.png"
+        content="https://img.icons8.com/office/100/000000/anime-emoji.png"
       />
       <meta name="msapplication-TileColor" content="#C0FFEE"></meta>
     </Head>
