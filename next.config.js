432fdc7bf8e43615b3eeb34e274af3b44e8cf98d
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-08-16 23:41:02 +0700

    add: kurang tau

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const withPWA = require("next-pwa");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-};
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
 
 module.exports = nextConfig;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,152 +1,157 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-const Document = () => (
-  <Html>
-    <Head>
-      <meta name="description" content="Kokunime" />
-      <meta charSet="utf-8" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta
-        name="keywords"
-        content="Kokunime, kokunime, website download anime, download batch anime, download anime, download anime terlengkap, website download anime terlengkap"
-      />
-      <meta name="theme-color" content="#ffc500" />
-      <link
-        type="image/png"
-        sizes="16x16"
-        rel="icon"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <link
-        type="image/png"
-        sizes="32x32"
-        rel="icon"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <link
-        type="image/png"
-        sizes="96x96"
-        rel="icon"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <link
-        type="image/png"
-        sizes="120x120"
-        rel="icon"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="72x72"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="144x144"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="512x512"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        type="image/png"
-        sizes="57x57"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        type="image/png"
-        sizes="60x60"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        type="image/png"
-        sizes="72x72"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        type="image/png"
-        sizes="76x76"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        type="image/png"
-        sizes="114x114"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        type="image/png"
-        sizes="120x120"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        type="image/png"
-        sizes="144x144"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        type="image/png"
-        sizes="152x152"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        type="image/png"
-        sizes="180x180"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <link
-        color="#26E07F"
-        rel="mask-icon"
-        href="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <meta
-        name="msapplication-square70x70logo"
-        content="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <meta
-        name="msapplication-TileImage"
-        content="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <meta
-        name="msapplication-square150x150logo"
-        content="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <meta
-        name="msapplication-square310x310logo"
-        content="https://img.icons8.com/office/100/000000/anime-emoji.png"
-      />
-      <meta name="msapplication-TileColor" content="#C0FFEE"></meta>
-    </Head>
-    <body>
-      <Main />
-      <NextScript />
-    </body>
-  </Html>
-);
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="description" content="Kokunime" />
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="keywords"
+            content="Kokunime, kokunime, website download anime, download batch anime, download anime, download anime terlengkap, website download anime terlengkap"
+          />
+          <meta name="theme-color" content="#ffc500" />
+          <link
+            type="image/png"
+            sizes="16x16"
+            rel="icon"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <link
+            type="image/png"
+            sizes="32x32"
+            rel="icon"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <link
+            type="image/png"
+            sizes="96x96"
+            rel="icon"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <link
+            type="image/png"
+            sizes="120x120"
+            rel="icon"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="72x72"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="96x96"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="144x144"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="512x512"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="57x57"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="60x60"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="72x72"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="76x76"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="114x114"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="120x120"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="144x144"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="152x152"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="180x180"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <link
+            color="#26E07F"
+            rel="mask-icon"
+            href="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <meta
+            name="msapplication-square70x70logo"
+            content="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <meta
+            name="msapplication-TileImage"
+            content="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <meta
+            name="msapplication-square150x150logo"
+            content="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <meta
+            name="msapplication-square310x310logo"
+            content="https://img.icons8.com/office/100/000000/anime-emoji.png"
+          />
+          <meta name="msapplication-TileColor" content="#C0FFEE"></meta>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
 
-export default Document;
+export default MyDocument;
