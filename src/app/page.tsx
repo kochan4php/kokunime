540dc36c8f170e66c94cd25eb94949de64c99be2
kochan4php <aprodeosubarno@gmail.com>
2023-06-11 08:55:43 +0700

    sedikit perbaikan kecil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@ import CardAnime from "@/components/CardAnime";
 import { axiosInstance } from "@/config";
 import { ResponseGetAllType } from "@/interfaces";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 export default async function Home(props: any): Promise<JSX.Element> {
     const page = props.searchParams.page || 1;
     const res = await axiosInstance.get(`/page/${page}`);
-    console.log(res);
-    const anime = res.data;
+    const anime = res.data.data.anime;
 
     return (
         <main className="min-h-full">
@@ -16,24 +14,24 @@ export default async function Home(props: any): Promise<JSX.Element> {
                 <section
                     className="bg-slate-800 min-w-full bg-cover bg-center bg-no-repeat object-cover"
                     style={{
-                        backgroundImage: `url(${anime.data[0]?.link?.image})`,
+                        backgroundImage: `url(${anime[0]?.link?.image})`,
                     }}
                 >
                     <div className="min-h-[40vh] sm:min-h-[50vh] md:min-h-[60vh] lg:min-h-[70vh] xl:min-h-[80vh] 2xl:min-h-[75vh] bg-slate-800 bg-opacity-30 backdrop-blur-lg backdrop-brightness-105 flex items-center justify-start px-2 md:px-6 pb-6">
                         <div className="container max-w-full px-3 lg:px-0 lg:max-w-3xl">
                             <div className="my-6">
                                 <h1 className="text-slate-50 text-2xl md:text-4xl font-bold mb-4 selection:bg-red-700 selection:text-red-200">
-                                    {anime.data[0]?.title}
+                                    {anime[0]?.title}
                                 </h1>
                             </div>
                             <div className="my-6">
                                 <p className="text-lg md:text-2xl text-slate-200 font-semibold truncate selection:bg-teal-400 selection:text-teal-900">
-                                    {anime.data[0]?.release}
+                                    {anime[0]?.release}
                                 </p>
                             </div>
                             <div className="my-6">
                                 <Link
-                                    href={`/anime/${anime.data[0]?.link?.endpoint}`}
+                                    href={`/anime/${anime[0]?.link?.endpoint}`}
                                     passHref
                                 >
                                     <button className="px-1 py-1.5 bg-slate-900 rounded-full text-slate-5 active:ring active:ring-sky-500 hover:border-sky-500 border-2 border-transparent transition-all duration-300 selection:bg-orange-500 selection:text-orange-900 text-base md:text-lg font-semibold w-full h-full">
@@ -53,7 +51,7 @@ export default async function Home(props: any): Promise<JSX.Element> {
                         </div>
                         <div className="container px-3 md:px-4">
                             <div className="grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 lg:gap-6">
-                                {anime.data.map(
+                                {anime.map(
                                     (
                                         item: ResponseGetAllType,
                                         index: number
