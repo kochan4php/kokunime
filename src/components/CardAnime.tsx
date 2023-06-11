9cf6f98c3e819aaf740813bc20e3cdb795c5b5e5
kochan4php <aprodeosubarno@gmail.com>
2023-06-11 15:52:01 +0700

    sedikit perbaikan kecil

--- src/components/CardAnime.tsx
+++ src/components/CardAnime.tsx
@@ -2,34 +2,32 @@ import { CardAnimeProps } from "@/interfaces";
 import Image from "next/image";
 import Link from "next/link";
 
-const CardAnime = ({ src, alt, title, path }: CardAnimeProps): JSX.Element => {
-    return (
-        <div className="bg-cover bg-center h-full bg-no-repeat selection:bg-violet-500 rounded overflow-hidden bg-slate-700">
-            <div className="min-w-full h-full py-4">
-                <Link href={path}>
-                    <div className="cursor-pointer group px-2 relative min-h-full">
-                        <div className="group px-2 relative min-h-full">
-                            <Image
-                                src={src}
-                                alt={alt || "thumbnail"}
-                                width={0}
-                                height={0}
-                                sizes="100vw"
-                                style={{ width: "100%", height: "auto" }}
-                                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAY"
-                                placeholder="blur"
-                            />
-                            <p
-                                className={`text-white group-hover:text-teal-300 tracking-wide transition-colors duration-300 selection:bg-teal-500 selection:text-teal-800 block font-semibold pt-4`}
-                            >
-                                {title}
-                            </p>
-                        </div>
+const CardAnime = ({ src, alt, title, path }: CardAnimeProps): JSX.Element => (
+    <div className="bg-cover bg-center h-full bg-no-repeat selection:bg-violet-500 rounded overflow-hidden bg-slate-700">
+        <div className="min-w-full h-full py-4">
+            <Link href={path}>
+                <div className="cursor-pointer group px-2 relative min-h-full">
+                    <div className="group px-2 relative min-h-full">
+                        <Image
+                            src={src}
+                            alt={alt || "thumbnail"}
+                            width={0}
+                            height={0}
+                            sizes="100vw"
+                            style={{ width: "100%", height: "auto" }}
+                            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAY"
+                            placeholder="blur"
+                        />
+                        <p
+                            className={`text-white group-hover:text-teal-300 tracking-wide transition-colors duration-300 selection:bg-teal-500 selection:text-teal-800 block font-semibold pt-4`}
+                        >
+                            {title}
+                        </p>
                     </div>
-                </Link>
-            </div>
+                </div>
+            </Link>
         </div>
-    );
-};
+    </div>
+);
 
 export default CardAnime;
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -8,18 +8,16 @@ const Input = ({
     autoComplete,
     value,
     onChange,
-}: InputProps): JSX.Element => {
-    return (
-        <input
-            className={`search-input truncate outline-none px-5 py-2 md:py-1 rounded-full bg-slate-800 text-base ring-2 focus:ring-4 focus:ring-sky-500 transition-all selection:bg-rose-700 selection:text-rose-300 font-bold ${width}`}
-            type={type}
-            name={name}
-            placeholder={placeholder}
-            autoComplete={autoComplete}
-            value={value}
-            onChange={onChange}
-        />
-    );
-};
+}: InputProps): JSX.Element => (
+    <input
+        className={`search-input truncate outline-none px-5 py-2 md:py-1 rounded-full bg-slate-800 text-base ring-2 focus:ring-4 focus:ring-sky-500 transition-all selection:bg-rose-700 selection:text-rose-300 font-bold ${width}`}
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        autoComplete={autoComplete}
+        value={value}
+        onChange={onChange}
+    />
+);
 
 export default Input;
