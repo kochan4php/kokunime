c7845a8db425496dfbc1cf5d431ce7604d56d566
kochan4php <aprodeosubarno@gmail.com>
2023-06-24 20:36:49 +0700

    benerin indentasi dari 4 jadi 2

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,6 @@ import axios, { AxiosInstance } from "axios";
 export const KUSONIME_API: string = "https://kusonime-restapi.vercel.app/api";
 
 export const axiosInstance: AxiosInstance = axios.create({
-    baseURL: KUSONIME_API,
-    headers: { "User-Agent": "*", Connection: "keep-alive" },
+  baseURL: KUSONIME_API,
+  headers: { "User-Agent": "*", Connection: "keep-alive" },
 });
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,61 +1,61 @@
 import React from "react";
 
 export interface ChildrenProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export interface InputProps {
-    width?: string;
-    type: string;
-    name: string;
-    placeholder: string;
-    autoComplete: string;
-    value: string;
-    onChange: (e: any) => void;
+  width?: string;
+  type: string;
+  name: string;
+  placeholder: string;
+  autoComplete: string;
+  value: string;
+  onChange: (e: any) => void;
 }
 
 export interface CardAnimeProps {
-    src: string;
-    alt?: string;
-    title: string;
-    path: string;
+  src: string;
+  alt?: string;
+  title: string;
+  path: string;
 }
 
 export interface ResponseGetAllType {
-    title: string;
-    release: string;
-    genres: string[];
-    link: {
-        endpoint: string;
-        url: string;
-        image: string;
-    };
+  title: string;
+  release: string;
+  genres: string[];
+  link: {
+    endpoint: string;
+    url: string;
+    image: string;
+  };
 }
 
 export interface ResponseGetRekomendasiType {
-    title: string;
-    endpoint: string;
-    image: string;
-    url: string;
+  title: string;
+  endpoint: string;
+  image: string;
+  url: string;
 }
 
 export interface AnimeGenresType {
-    name: string;
-    url: string;
-    endpoint: string;
+  name: string;
+  url: string;
+  endpoint: string;
 }
 
 export interface AnimeDownloadOptionType {
-    title: string;
-    link_download: AnimeLinkDownloadType[];
+  title: string;
+  link_download: AnimeLinkDownloadType[];
 }
 
 export interface AnimeLinkDownloadType {
-    resolusi: string;
-    link: AnimeLinkPlatformType[];
+  resolusi: string;
+  link: AnimeLinkPlatformType[];
 }
 
 export interface AnimeLinkPlatformType {
-    platform: string;
-    url: string;
+  platform: string;
+  url: string;
 }
