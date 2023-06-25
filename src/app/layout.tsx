842dd3677022bb8bc9836163447e3734f7dd02c3
kochan4php <aprodeosubarno@gmail.com>
2023-06-25 13:45:36 +0700

    update versi next js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { ChildrenProps } from "@/interfaces";
-import { nunito } from "@/utils/fonts";
 import "./globals.css";
 
 export const metadata = {
@@ -10,7 +9,7 @@ export const metadata = {
 };
 
 const RootLayout = ({ children }: ChildrenProps): JSX.Element => (
-  <html lang="en" className={`${nunito.className} scroll-smooth`}>
+  <html lang="en" className="scroll-smooth">
     <body
       suppressHydrationWarning
       className="!overflow-x-hidden bg-gradient-to-tl from-slate-900 via-slate-800 to-slate-900 min-h-screen flex flex-col justify-between"
