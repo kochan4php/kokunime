147285b90c81c368547c90aaf9219f2f99ced856
kochan4php <aprodeosubarno@gmail.com>
2023-06-25 15:02:14 +0700

    perbaikan kecil pada halaman hasil pencarian

--- src/app/search/[input]/page.tsx
+++ src/app/search/[input]/page.tsx
@@ -13,7 +13,7 @@ const SearchAnime = async (props: any): Promise<JSX.Element> => {
     <section className="min-w-full pt-4 pb-8">
       <div className="container flex items-center pt-4 md:pt-2 px-4 mb-5 md:px-0">
         <h1 className="text-3xl md:text-4xl text-sky-300 selection:bg-emerald-500 selection:text-emerald-900 font-bold">
-          Hasil pencarian {input.split("%2B").join(" ")}
+          Hasil pencarian kamu :
         </h1>
       </div>
       <div className="container px-4 md:px-0 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 lg:gap-6 my-3">
