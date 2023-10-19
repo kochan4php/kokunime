af292a96d193adac3149deb58e334791cb1525af
kochan4php <aprodeosubarno@gmail.com>
2023-10-19 14:36:20 +0700

    add open graph to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,39 @@
 import { ChildrenProps } from "@/interfaces";
+import { Metadata } from "next";
 import NextTopLoader from "nextjs-toploader";
 import { Suspense } from "react";
 import "./globals.css";
 import Loading from "./loading";
 
-export const metadata = {
+export const metadata: Metadata = {
     title: "Kokunime",
     description: "Website download anime dan baca manga",
+    openGraph: {
+        title: "Kokunime",
+        description: "Website buat download anime dan baca manga tanpa iklan 😎",
+        url: "https://kokunime.netlify.app",
+        siteName: "Kokunime",
+        images: [
+            {
+                url: "https://kokunime.netlify.app/farhan-kebab.jpg",
+                alt: "Alt nya kalo preview gambarnya ga ada hehe",
+            },
+            {
+                url: "https://kokunime.netlify.app/ironi-mamah-aku-takut.jpg",
+                alt: "Alt nya kalo preview gambarnya ga ada hehe",
+            },
+            {
+                url: "https://kokunime.netlify.app/haah-lah.jpg",
+                alt: "Alt nya kalo preview gambarnya ga ada hehe",
+            },
+            {
+                url: "https://kokunime.netlify.app/maxresdefault.jpg",
+                alt: "Alt nya kalo preview gambarnya ga ada hehe",
+            },
+        ],
+        locale: "en_US",
+        type: "website",
+    },
 };
 
 const RootLayout = ({ children }: ChildrenProps): JSX.Element => (
