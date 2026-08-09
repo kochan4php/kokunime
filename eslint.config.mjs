6de1d437917bda18005da685481bc3816a1d884c
kochan4php <aprodeosubarno@gmail.com>
2026-08-09 14:14:31 +0700

    upgrade to nextjs 16, react 19.2, tailwind 4 and fix build

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import nextVitals from "eslint-config-next/core-web-vitals";
+
+const config = [
+  ...nextVitals,
+  {
+    ignores: [".next/**", "node_modules/**"],
+  },
+];
+
+export default config;
